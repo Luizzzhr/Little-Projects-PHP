a5f8cf9fd15ab4241180fe07aa423a39df858228
Luizzzhr <90866175+Luizzzhr@users.noreply.github.com>
2022-01-11 17:11:37 -0300

    Update exercicio03.php

--- exercicio03.php
+++ exercicio03.php
@@ -11,7 +11,7 @@
         $r1 = isset($_GET["n1"])?($_GET["n1"]):0;
 		$r2 = isset($_GET["n2"])?$_GET["n2"]:0;
 		$m = ($r1+$r2)/2;
-		echo "A media entre $r1 e $r2 é $m";
+		echo "A media entre <span class='foco'>$r1</span> e <span class='foco'>$r2</span class> é <span class='foco'>$m</span>";
 			if($m<5){
 				
 				$situacao="REPROVADO";
@@ -26,11 +26,11 @@
 				$situacao="APROVADO";
 				
 			}
-			echo " </br></br>Ele estar $situacao";
+			echo "</br></br>Ele estar <span class='foco'>$situacao</span>";
 			
 			
     ?>
+	</br></br><a href="javascript:history.go(-1)" class="botao">Voltar</a>
 </div>
 </body>
 </html>
- 
